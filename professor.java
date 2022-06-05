1636dd1bac3ae01c52db3f49fa64b4c4df380123
SabbathCode <daniel.arturjsb@gmail.com>
2022-06-04 23:49:29 -0300

    baruque & baruque

---
+++ professor.java
@@ -0,0 +1,16 @@
+class Data{
+    String admissao;
+}
+
+
+class Professor{
+    String nome;
+    String departamento;
+    String registro;
+    Data admissao;
+
+
+    public static void main(String[] args){
+
+    }
+}
