ba0b818c384fd19830f836fabf82579a863dac01
SabbathCode <daniel.arturjsb@gmail.com>
2022-06-05 00:03:54 -0300

    baruque & Baruque 4 e 5

--- professor.java
+++ professor.java
@@ -1,7 +1,29 @@
-class Data{
+public class Data{
     String admissao;
-}
+    int dia;
+    int mes;
+    int ano;
 
+    public Data(int dia, int mes, int ano){
+    
+        if(dia> 0 && dia<=31){
+            this.dia = dia;
+        } else{
+            System.out.println("dia invalido")
+        }
+        if(mes > 0 && mes<=12){
+            this.mes = mes;
+        } else{
+            System.out.println("mes invalido")
+        }
+        if(ano> 0){
+            this.ano = ano;
+            System.out.println("data valida")
+        }else{
+            System.out.println("ano invalido")
+        }
+    }
+}
 
 class Professor{
     String nome;
